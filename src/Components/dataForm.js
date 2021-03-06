3ac1bb5b7d8f601d5c963363c40e1e9e67bfdbf6
sbonafe <60677766+sbonafe@users.noreply.github.com>
2021-03-06 12:42:53 -0700

    Update dataForm.js

--- src/Components/dataForm.js
+++ src/Components/dataForm.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import {Button, InputGroup, InputGroupAddon, InputGroupText, Input, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 import React, { Component } from 'react';
 
 class dataForm extends Component{
-    state = {};
+    constructor(props){
+        super(props);
+        this.findToggleNew = this.findToggleNew.bind(this);
+
+        this.state = {
+            matchName:"",
+            modalNew: false,
+        }
+    }
 
 
     render(){
-        
+        <div>
+            <Button color='success' onClick={this.findToggleNew} className="mb-1" block>Create Event</Button>
+                <Modal isOpen={this.state.modalNew} toggle={this.findToggleNew}>
+                    <ModalHeader toggle={this.findToggleNew}>Create Event</ModalHeader>
+                    <ModalBody>
+                        {/* InputGroup here */}
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>Create</InputGroupText>
+                            </InputGroupAddon>
+                                <Input
+                                    placeholder="Event Name"
+                                    onChange={this.functionTakingMatchInput}
+                                />
+                            
+                        </InputGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={this.fetchFind} color="success">Add</Button>
+                        <Button color='secondary' onClick={this.findToggleNew}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+
+        </div>
+    }
+
+    findToggleNew() {
+        this.setState({
+            modalNew: !this.state.modalNew
+        });
+    }
+
+    functionTakingMatchInput = (event) =>{
+        this.setState({matchName: event.target.value})
     }
 }
 export default dataForm;
