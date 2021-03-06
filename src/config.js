a67f7931ac6ba956abbbec699a2e0ab706dee30b
Karthik Palusa <karthik.palusa@gmail.com>
2021-03-06 11:08:20 -0700

    Authentication

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyA-sfwXQoIw4jZ_4NJlPolrXW8xV2_WxgY",
+  authDomain: "hackcu21.firebaseapp.com",
+  databaseURL: "https://hackcu21-default-rtdb.firebaseio.com",
+  projectId: "hackcu21",
+  storageBucket: "hackcu21.appspot.com",
+  messagingSenderId: "523512605214",
+  appId: "1:523512605214:web:a1f6c37227f10250481c0f",
+  measurementId: "G-TPCTZG01K4"
+};
