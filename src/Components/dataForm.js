dd6d18a9878642a63c6904a6d3733a6974a59cda
sbonafe <60677766+sbonafe@users.noreply.github.com>
2021-03-06 11:45:27 -0700

    Create dataForm.js

---
+++ src/Components/dataForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import React, { Component } from 'react';
+
+class dataForm extends Component{
+    state = {};
+
+
+    render(){
+        
+    }
+}
+export default dataForm;
