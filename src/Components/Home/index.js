e4a65019c21470e3c04430a7d8177e4d10245f62
Karthik Palusa <karthik.palusa@gmail.com>
2021-03-06 21:43:27 -0700

    clean up errors/code

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
-import {Button, InputGroup, InputGroupAddon, InputGroupText, Input, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
-import DataForm from './DataForm'
+import React from "react";
+import DataForm from "./DataForm";
 
-import { withAuthorization } from '../Session';
+import { withAuthorization } from "../Session";
 
 const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-    <p><DataForm></DataForm></p>
-  </div>
+	<div>
+		<h1> Home Page </h1>{" "}
+		<p> The Home Page is accessible by every signed in user. </p>{" "}
+		<p>
+			{" "}
+			<DataForm> </DataForm>
+		</p>
+	</div>
 );
 
-const condition = authUser => !!authUser;
+const condition = (authUser) => !!authUser;
 
 export default withAuthorization(condition)(HomePage);
