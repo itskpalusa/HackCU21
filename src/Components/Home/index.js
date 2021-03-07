6392be4d05c8a500f3765108354103cca80f5a9e
Karthik Palusa <karthik.palusa@gmail.com>
2021-03-07 00:26:38 -0700

    create table of data from events

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import DataForm from "./DataForm";
 
 import { withAuthorization } from "../Session";
-
+import Table from "./Table";
 const HomePage = () => (
 	<div>
 		<h1> Home Page </h1>{" "}
 		<p> The Home Page is accessible by every signed in user. </p> <DataForm />
+		<Table />
 	</div>
 );
 
